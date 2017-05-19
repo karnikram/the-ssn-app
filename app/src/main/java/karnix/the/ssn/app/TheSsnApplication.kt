0482eb6cd8eec9b7574bf888f13f48d9a908cda6
Adithya J <adithya.j@adithya321.me>
2017-05-19 11:46:57 +0530

    :tada: Configure Kotlin in Project

---
+++ app/src/main/java/karnix/the/ssn/app/TheSsnApplication.kt
@@ -0,0 +1,25 @@
+package karnix.the.ssn.app
+
+import android.app.Application
+import android.os.StrictMode
+
+import com.facebook.stetho.Stetho
+
+import karnix.the.ssn.ssnmachan.BuildConfig
+
+/**
+ * Created by adithya321 on 4/20/17.
+ */
+
+class TheSsnApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.enableDefaults()
+
+            Stetho.initializeWithDefaults(this)
+        }
+    }
+}
